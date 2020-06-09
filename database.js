b5295aeb0b06a9b3a7d63997f5497f8865cf9f87
Rémi Mavillaz <remimavillaz@macbook-pro-de-remi-2.home>
2020-06-09 21:59:12 +0200

    add prod db

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
 const config = require('./knexfile');
 
+const environment = process.env.NODE_ENV || 'local';
 
 const database = require('knex')({
   pool: { min: 0, max: 5 },
-  ...config['dev'],
+  ...config[environment],
 });
 
 module.exports = database;
--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,37 @@
 'use strict';
 
 const config = {
+  local: {
+    client: 'pg',
+    connection: {
+      host: 'bibuterie-db-dev.cygwulxkyoyy.eu-west-1.rds.amazonaws.com',
+      user: 'postgres',
+      password: 'toCxT9lNYcDKvTRUeN0x',
+      database: 'postgres',
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/dev',
+    },
+  },
   dev: {
+    client: 'pg',
+    connection: {
+      host: 'bibuterie-db-dev.cygwulxkyoyy.eu-west-1.rds.amazonaws.com',
+      user: 'postgres',
+      password: 'toCxT9lNYcDKvTRUeN0x',
+      database: 'postgres',
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/dev',
+    },
+  },
+  production: {
     client: 'pg',
     connection: {
       host: 'bibuterie-db-dev.cygwulxkyoyy.eu-west-1.rds.amazonaws.com',
